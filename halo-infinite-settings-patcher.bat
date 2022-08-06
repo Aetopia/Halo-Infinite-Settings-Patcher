beb36e3245dc47a1baac3ae25ab09fc0032aabf1
Aetopia <kirbymario22@gmail.com>
2022-08-06 22:48:01 +0530

    Update halo-infinite-settings-patcher.bat

--- halo-infinite-settings-patcher.bat
+++ halo-infinite-settings-patcher.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Fetch Files
-curl.exe -L# "https://raw.githubusercontent.com/Aetopia/Halo-Infinite-Settings-Patcher/main/halo-infinite-settings-patcher.py" -o "%TEMP%\halo-infinite-settings-optimizer.py"
+curl.exe -L# "https://raw.githubusercontent.com/Aetopia/Halo-Infinite-Settings-Patcher/main/halo-infinite-settings-patcher.py" -o "%TEMP%\halo-infinite-settings-patcher.py"
 curl.exe -L# "https://raw.githubusercontent.com/Aetopia/Halo-Infinite-Settings-Patcher/main/settings.json" -o "%TEMP%\settings.json"
 curl.exe -L# "https://github.com/Aetopia/PyOne-Onefile-Python-Interpreter/releases/download/v3.10/pyone.exe" -o "%TEMP%\pyone.exe"
 
