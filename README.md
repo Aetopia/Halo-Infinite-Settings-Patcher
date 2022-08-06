4a9c77561a9cff1cc38aaabb35a6b4b4159f8bf5
Aetopia <kirbymario22@gmail.com>
2022-08-06 22:43:17 +0530

    Upload Files.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Halo Infinite Settings Patcher
  
+A simple settings patcher for Halo Infinite.
+Simply run the batch file and you are good to go!
---
+++ halo-infinite-settings-optimizer.py
@@ -0,0 +1,32 @@
+import json
+import os
+from traceback import print_exc
+from getpass import getpass
+
+def main():
+    LOCALAPPDATA= os.getenv("LOCALAPPDATA")
+    uwp = f'{LOCALAPPDATA}\Packages\Microsoft.254428597CFE2_8wekyb3d8bbwe\LocalCache\Local\HaloInfinite\Settings\SpecControlSettings.json'
+    steam = f'{LOCALAPPDATA}\HaloInfinite\Settings\SpecControlSettings.json'
+
+    with open(f'{os.path.dirname(__file__)}/settings.json', 'r', encoding='utf-8') as f:
+        settings = json.load(f)
+
+    for file in (uwp, steam):
+        if os.path.exists(file):
+            break
+
+    with open(file, 'r+', encoding='utf-8') as f:
+        contents = json.load(f)
+        f.seek(0)
+        for key, value in settings.items():
+            print(key, value)
+            contents[key] = value
+        f.write(json.dumps(contents, indent=4))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print_exc()
+    getpass('Press [Enter] to exit...')
